3d23ebc3a953abd09f230151ceab5f2722e4dad6
gyurisc <gyurisc@gmail.com>
2020-04-24 13:13:59 +0200

    setting up mdx

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export const Error = () => {
+    return (
+        <Layout>
+            <h1>This is our Error Page</h1>
+        </Layout>
+    )
+}
